70a820f1390fc61d7c7e8ae6044b80ac59f294c3
taleore <taleore@gmail.com>
2019-09-11 17:00:03 +0200

    fix Pokemon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pokemons from "./Pokemons";
+import Pokemon from "./components/Pokemon";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends React.Component {
           <h1 className="App-title">Mi Lista de Pokemon</h1>
         </header>
         <main className="App-main"></main>
+        <Pokemon data={pokemons} />
       </div>
     );
   }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+
 class Pokemon extends React.Component {
-  constructor(props) {
-    super(props);
+    render() {
+    return (
+      <div className="card-container">
+        <img className="card-image" src={this.props.url}></img>
+        <h2 className="card-title">{this.props.name}</h2>
+        <ul className="card-list">{this.props.types.map((type,index) => return(<li key={index} className="card-type">{type}</li>);
+        })}
+        </ul>
+      </div>
+    );
   }
 }
+
+
+export default Pokemon;
+
