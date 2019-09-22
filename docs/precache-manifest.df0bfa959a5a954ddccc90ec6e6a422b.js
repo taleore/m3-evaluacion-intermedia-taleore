0c24581ef0b60fc00dc4647eeb8621fefca3cf30
taleore <taleore@gmail.com>
2019-09-23 00:56:56 +0200

    add githubpages

---
+++ docs/precache-manifest.df0bfa959a5a954ddccc90ec6e6a422b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d713851a41a49f64ca765a86e3b44c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "749366481605b3c0dcfe",
+    "url": "./static/js/2.10857717.chunk.js"
+  },
+  {
+    "revision": "8f9ea8b3237a5f821c35",
+    "url": "./static/js/main.94e601c5.chunk.js"
+  },
+  {
+    "revision": "8090f3568270586d1fdb",
+    "url": "./static/js/runtime~main.0286081e.js"
+  }
+]);
